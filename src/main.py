bddafc04911d0f954964396feec5af5f6aac305b
Claudia <88203931+clauculus@users.noreply.github.com>
2022-04-04 21:58:13 +0700

    finish debugging

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 from puzzle import Puzzle
 import time
+import copy
 
 # Main Program
 
 print()
-print("=====WELCOME TO 15 PUZZLE SOLVER!=====")
+print("========================================")
+print("======WELCOME TO 15 PUZZLE SOLVER!======")
+print("========================================")
 print()
+
 print("*** DO YOU WANT TO USE INPUT FILE? (Y/N)")
 
 # Asumsi input user selalu benar
 inputUser = input()
+print()
 
 puzzle = Puzzle()
 
@@ -19,22 +24,38 @@ if (inputUser == "Y"):
 else:
     puzzle.random()
 
+printpuzzle = copy.deepcopy(puzzle)
+
 print()
-print("=====PUZZLE's INITIAL STATE=====")
+print("========PUZZLE's INITIAL STATE==========")
 puzzle.print()
 print()
 
 if (puzzle.isSolvable()):
+
     startTime = time.time()
 
     puzzle.generated.append(puzzle.puzzle)
 
     while(not puzzle.isSolution()):
-        puzzle.solvePuzzle()
+       puzzle.solvePuzzle()
 
     endTime = time.time()    
+    n = len(puzzle.path)
+
+    for path in puzzle.path:
+        if (path == 'up'):
+            printpuzzle.printup()
+        elif (path == 'down'):
+            printpuzzle.printdown()
+        elif (path == 'left'):
+            printpuzzle.printleft()
+        else:
+            printpuzzle.printright()
+
 
     print("PUZZLE IS SOLVED!")
+    print("TOTAL STEPS : ", n)
     print("TOTAL NODE GENERATED: ", Puzzle.node)
     print("TIME TAKEN: ",endTime - startTime,"s")
 
--- src/puzzle.py
+++ src/puzzle.py
@@ -1,9 +1,7 @@
 import random
 import copy
-from turtle import right
 
 class Puzzle: 
-    depth = 0
     node = 0
 
     # cctor
@@ -11,6 +9,8 @@ class Puzzle:
         self.puzzle = [0 for i in range(16)]
         self.queue = []
         self.generated = []
+        self.path = []
+        self.depth = 0
 
     # to randomize puzzle
     def random(self):
@@ -34,6 +34,42 @@ class Puzzle:
             if (i % 4 == 3):
                 print()
 
+    def printup(self):
+        idx = self.find16()
+        if (idx > 3):
+            self.puzzle[idx] = self.puzzle[idx - 4]
+            self.puzzle[idx - 4] = 16 
+        print('up')
+        self.print()
+        print()
+
+    def printdown(self):
+        idx = self.find16()
+        if (idx < 12):
+            self.puzzle[idx] = self.puzzle[idx + 4]
+            self.puzzle[idx + 4] = 16
+        print('down')
+        self.print()
+        print()
+
+    def printleft(self):
+        idx = self.find16()
+        if (idx % 4 != 0):
+            self.puzzle[idx] = self.puzzle[idx - 1]
+            self.puzzle[idx - 1] = 16
+        print('left')
+        self.print()
+        print()        
+
+    def printright(self):
+        idx = self.find16()
+        if ( idx % 4 != 3):
+            self.puzzle[idx] = self.puzzle[idx + 1]
+            self.puzzle[idx + 1] = 16
+        print('right')
+        self.print()
+        print()        
+            
     # to count the number of misplaced puzzle
     def misplaced(self, puz):
         count = 0
@@ -42,12 +78,7 @@ class Puzzle:
                 if (puz[i] != 16):
                     count += 1
         return count
-    
-    # to count the cost
-    def cost(self, puz):
-        #print("Depth adalah ", Puzzle.depth)
-        #print("Banyak misplaced ", self.misplaced(puz))
-        return Puzzle.depth + self.misplaced(puz)
+
 
     # to find KURANG (i)
     def kurang(self, i):
@@ -70,12 +101,20 @@ class Puzzle:
             print("KURANG (" + str(i + 1) + ") = "+ str(kurang[i]))
             
         idx = [1, 3, 4, 6, 9, 11, 12, 14]
+        x = False
         for i in idx:
             if (self.puzzle[i] == 16):
-                print("X = 1")
+                x = True
                 total += 1
 
+        if (x):
+            print("X = 1")
+        else:
+            print("X = 0")
+
         print("SIGMA of KURANG(i) + X = ", total)
+        print()
+        print("Loading...")
         print()
         
         if (total % 2 == 0):
@@ -92,7 +131,7 @@ class Puzzle:
 
         return solution
 
-    # return index of 16 (blank space)
+    # return the index of 16 (blank space)
     def find16(self):
         i = 0
         while (self.puzzle[i] != 16):
@@ -100,6 +139,7 @@ class Puzzle:
 
         return i
 
+    # check is it possible to move up
     def isUp(self):
         idx = self.find16()
         if ( idx > 3):
@@ -111,11 +151,12 @@ class Puzzle:
     def up(self):
         upPuzzle = copy.deepcopy(self.puzzle)
         idx = self.find16()
-        if ( idx > 3):
+        if (idx > 3):
             upPuzzle[idx] = upPuzzle[idx - 4]
             upPuzzle[idx - 4] = 16
         return upPuzzle
 
+    # check is it possible to move down
     def isDown(self):
         idx = self.find16()
         if ( idx < 12):
@@ -127,11 +168,12 @@ class Puzzle:
     def down(self):
         downPuzzle = copy.deepcopy(self.puzzle)
         idx = self.find16()
-        if ( idx < 12):
+        if (idx < 12):
             downPuzzle[idx] = downPuzzle[idx + 4]
             downPuzzle[idx + 4] = 16
         return downPuzzle
-    
+
+    # check is it possible to move left    
     def isLeft(self):
         idx = self.find16()
         if (idx % 4 != 0):
@@ -148,6 +190,7 @@ class Puzzle:
             leftPuzzle[idx - 1] = 16
         return leftPuzzle 
     
+    # check is it possible to move right
     def isRight(self):
         idx = self.find16()
         if (idx % 4 != 3):
@@ -171,33 +214,53 @@ class Puzzle:
         else:
             return False
     
+    # branch and bound
     def solvePuzzle(self):
+        
+        self.depth += 1
+
+        # check if it is possible to move up
         if (self.isUp()):
             upPuzzle = self.up()     
+            # if the state has not been generated before
             if (not self.isGenerated(upPuzzle)):
+                path = copy.deepcopy(self.path)
+                path.append('up')
                 self.generated.append(upPuzzle)
-                self.queue.append([self.cost(upPuzzle), upPuzzle, 'up'])
+                self.queue.insert(0,[self.misplaced(upPuzzle) + self.depth, upPuzzle, path, self.depth])
                 Puzzle.node += 1 
 
+        # check if it is possible to move right
         if (self.isRight()):
             rightPuzzle = self.right() 
+            # if the state has not been generated before
             if (not self.isGenerated(rightPuzzle)):
+                path = copy.deepcopy(self.path)
+                path.append('right')
                 self.generated.append(rightPuzzle)
-                self.queue.append([self.cost(rightPuzzle), rightPuzzle, 'right'])
+                self.queue.insert(0,[self.misplaced(rightPuzzle) + self.depth, rightPuzzle, path, self.depth])
                 Puzzle.node += 1          
 
+        # check if it is possible to move down
         if (self.isDown()):
             downPuzzle = self.down()   
+            # if the state has not been generated before
             if (not self.isGenerated(downPuzzle)):
+                path = copy.deepcopy(self.path)
+                path.append('down')
                 self.generated.append(downPuzzle)
-                self.queue.append([self.cost(downPuzzle), downPuzzle, 'down'])
+                self.queue.insert(0,[self.misplaced(downPuzzle) + self.depth, downPuzzle, path, self.depth])
                 Puzzle.node += 1    
 
+        # check if it is possible to move left
         if (self.isLeft()):
             leftPuzzle = self.left()
+            # if the state has not been generated before
             if (not self.isGenerated(leftPuzzle)):
+                path = copy.deepcopy(self.path)
+                path.append('left')
                 self.generated.append(leftPuzzle)
-                self.queue.append([self.cost(leftPuzzle), leftPuzzle, 'left'])
+                self.queue.insert(0,[self.misplaced(leftPuzzle) + self.depth, leftPuzzle, path, self.depth])
                 Puzzle.node += 1     
 
         self.queue.sort()
@@ -205,11 +268,8 @@ class Puzzle:
         current = self.queue.pop(0)
 
         self.puzzle = current[1]
-        print(current[2])
-        self.print()
-        print()
-      
-
+        self.path = current[2]
+        self.depth = current[3]
 
 
 
